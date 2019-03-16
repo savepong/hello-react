9dd5241d76926caa4cfe0d5b8774198ae9e30451
Pongsiri Chuaychoonoo <savepong.com@gmail.com>
2019-03-17 01:11:10 +0700

    Updates

---
+++ precache-manifest.6fec9dd8165bdaa268c913d3f6180bf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69dcf12e7815e7046f81",
+    "url": "/hello-react/static/js/runtime~main.d4bee3ea.js"
+  },
+  {
+    "revision": "6f5bc2aac774503abda3",
+    "url": "/hello-react/static/js/main.55ed3a99.chunk.js"
+  },
+  {
+    "revision": "37633691b9724b784dbc",
+    "url": "/hello-react/static/js/2.ff439d2d.chunk.js"
+  },
+  {
+    "revision": "6f5bc2aac774503abda3",
+    "url": "/hello-react/static/css/main.a1d5bb92.chunk.css"
+  },
+  {
+    "revision": "bb6cf068382d034829beb2ae9213f3fb",
+    "url": "/hello-react/index.html"
+  }
+];
