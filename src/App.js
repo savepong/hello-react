f6dbcb277c142de868be7fa1dfe80b7c431924b3
Pongsiri Chuaychoonoo <savepong.com@gmail.com>
2019-03-17 00:33:53 +0700

    Created Form component.

--- src/App.js
+++ src/App.js
@@ -3,24 +3,7 @@ import Table from './Table'
 
 class App extends Component {
     state = {
-        characters: [
-            {
-                name: 'Charlie',
-                job: 'Janitor',
-            },
-            {
-                name: 'Mac',
-                job: 'Bouncer',
-            },
-            {
-                name: 'Dee',
-                job: 'Aspring actress',
-            },
-            {
-                name: 'Dennis',
-                job: 'Bartender',
-            }
-        ]
+        characters: []
     };
 
     removeCharacter = index => {
@@ -35,7 +18,7 @@ class App extends Component {
 
     render() {
         const { characters } = this.state
-        
+
         return (
             <div className="container">
                 <Table characterData={characters} removeCharacter={this.removeCharacter} />
