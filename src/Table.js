a8584fc5b7fb96ff138e21f0be9acf0eb0f9a72c
Pongsiri Chuaychoonoo <savepong.com@gmail.com>
2019-03-17 00:19:44 +0700

    Map through the array

--- src/Table.js
+++ src/Table.js
@@ -11,8 +11,17 @@ const TableHeader = () => {
     )
 }
 
-const TableBody = () => {
-    return <tbody />
+const TableBody = props => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        )
+    })
+
+    return <tbody>{rows}</tbody>
 }
 
 class Table extends Component {
